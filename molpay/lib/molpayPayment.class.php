03c992ed32dfab716015fa52e87a4e6dc6e3b18b
Simon Tai <31197394+simontai828@users.noreply.github.com>
2017-08-22 09:22:54 +0800

    added IPN

--- molpay/lib/molpayPayment.class.php
+++ molpay/lib/molpayPayment.class.php
@@ -205,16 +205,47 @@ class molpayPayment extends waPayment implements waIPayment
         /********************************
         *Don't change below parameters
         ********************************/
+        $nbcb       =    $_POST['nbcb'];
         $tranID     =    $_POST['tranID'];
-        $orderid     =    $_POST['orderid'];
+        $orderid    =    $_POST['orderid'];
         $status     =    $_POST['status'];
-        $merchant     =    $_POST['domain'];
+        $merchant   =    $_POST['domain'];
         $amount     =    $_POST['amount'];
-        $currency     =    $_POST['currency'];
-        $appcode     =    $_POST['appcode'];
-        $paydate     =    $_POST['paydate'];
-        $skey     =    $_POST['skey']; //Security hashstring returned by MOLPay
-        
+        $currency   =    $_POST['currency'];
+        $appcode    =    $_POST['appcode'];
+        $paydate    =    $_POST['paydate'];
+        $skey       =    $_POST['skey']; //Security hashstring returned by MOLPay
+           
+        if ($nbcb==1) {
+           //callback IPN feedback to notified MOLPay
+             echo "CBTOKEN:MPSTATOK"; exit;
+        }else{
+             $_POST[treq]    =    1; // Additional parameter for IPN
+
+            /***********************************************************
+            * Snippet code in purple color is the enhancement required
+            * by merchant to add into their return script in order to
+            * implement backend acknowledge method for IPN
+            ************************************************************/
+            while ( list($k,$v) = each($_POST) ) {
+                $postData[]= $k."=".$v;
+                }
+                $postdata        = implode("&",$postData);
+                $url             = "https://www.onlinepayment.com.my/MOLPay/API/chkstat/returnipn.php";
+                $ch              = curl_init();
+                curl_setopt($ch, CURLOPT_POST                   , 1         );
+                curl_setopt($ch, CURLOPT_POSTFIELDS             , $postdata );
+                curl_setopt($ch, CURLOPT_URL                    , $url      );
+                curl_setopt($ch, CURLOPT_HEADER               , 1         );
+                curl_setopt($ch, CURLINFO_HEADER_OUT            , TRUE      );
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER         , 1         );
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER         , FALSE     );
+                curl_setopt($ch, CURLOPT_SSLVERSION           , 6         );  // use only TLSv1.2
+                $result = curl_exec( $ch );
+                curl_close( $ch );
+        }
+
+
         /***********************************************************
         * To verify the data integrity sending by MOLPay
         ************************************************************/
